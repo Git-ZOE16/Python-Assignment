71dbb95e6a503c18f47520d0a00c3b8d0297a469
Zoe <zoeinspired1609@gmail.com>
2026-05-10 22:18:47 +0100

    Printing prime numbers from 1-100

---
+++ Weekend Excercise/print_all_prime_numbers.py
@@ -0,0 +1,16 @@
+#Pseudocode:
+    #Step1. For every number, check for its prime number.
+    #Step2. If true, it is prime.
+
+count = 0
+
+for numbers in range(2, 101):
+    is_prime = True
+    for value in range(2, numbers):
+        if numbers % value == 0:
+            is_prime = False
+    if is_prime:
+        print(numbers)
+        count = count + 1
+
+print("Total primes:", count)
