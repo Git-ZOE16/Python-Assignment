9b4dc0c9a014e5b9cbd2c00e8ec73322fe9d9de1
Zoe <zoeinspired1609@gmail.com>
2026-05-10 21:44:25 +0100

    Word Count in a Sentence

---
+++ Weekend Excercise/counting_word_in_a_sentence.py
@@ -0,0 +1,8 @@
+#Pseudocode:
+    #Step1. Cut the sentence into pieces at every space.
+    #Step2. Count the pieces.
+
+line = " Oluyemi Zoe"
+list_of_words = line.split()
+print(len(list_of_words))
+
