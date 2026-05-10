777fa98a90f704eb335bfdef759d70d22022d2d2
Zoe <zoeinspired1609@gmail.com>
2026-05-10 19:39:35 +0100

    Finding the Average of Numbers from 1-100

---
+++ Weekend Excercise/average_of_numbers.py
@@ -0,0 +1,12 @@
+#Pseudocode:
+    #Step1. Add up all numbers from 1 to 100.
+    #Step2. Divide that total by 100.
+
+total = 0
+
+for numbers in range(1, 101):
+    total = total + numbers
+
+result = total / 100
+print(result)
+
