59ea21de6386cd0d9e1135434f780fc20ca0a76a
Zoe <zoeinspired1609@gmail.com>
2026-05-10 21:34:22 +0100

    Calculating the Base Exponent of a number

---
+++ Weekend Excercise/base_exponent.py
@@ -0,0 +1,13 @@
+#Pseudocode:
+    #Step1. Start with 1.
+    #Step2. Multiply by the base as many times as the exponent says.
+
+base = 5
+exponent = 2
+result = 1
+
+for i in range(exponent):
+    result = result * base
+
+print(result)
+
