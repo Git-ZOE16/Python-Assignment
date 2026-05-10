cd8edcde505f970154d9ecc709d5c5db68e3380b
Zoe <zoeinspired1609@gmail.com>
2026-05-10 08:06:27 +0100

    First Fifteen Multiples of Three numbers

---
+++ Afternoon Conflakes/first_fifteen_multiple_of_three.py
@@ -0,0 +1,10 @@
+#Pseudocode
+    #sTEP1. Create a loop that runs 15 times (from 1 to 15).
+    #Step2. Multiply the current loop count by 3.
+    #Step3. Print the result followed by a space, to keep it on the same line.
+    
+
+for i in range(1, 16):
+    multiple = i * 3
+    print(multiple, end=" ")
+
